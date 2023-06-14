e4c78476b46877a8b71c9422c11f15e296ef1639
Miguel Raatz <miguelraatz_@hotmail.com>
2023-06-14 01:39:50 -0300

    lint: correct

--- app/backend/src/utils/mapStatusHTTP.ts
+++ app/backend/src/utils/mapStatusHTTP.ts
@@ -1,11 +1,9 @@
-const map: Record<string, number> = {
-  SUCCESSFUL: 200,
-  UNAUTHORIZED: 401,
-  INVALID_DATA: 400,
-  NOT_FOUND: 404,
-  CONFLICT: 409,
-};
-
 export default function mapStatusHTTP(status: string): number {
-  return map[status] ?? 500;
+  switch (status) {
+    case 'SUCCESSFUL': return 200;
+    case 'INVALID_DATA': return 400;
+    case 'NOT_FOUND': return 404;
+    case 'CONFLICT': return 409;
+    default: return 500;
+  }
 }
